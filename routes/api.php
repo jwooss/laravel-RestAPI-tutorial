42d072dde24a53b2f1f0f8ade802eddddabb6db3
jwooss <wodn4131@gmail.com>
2019-02-16 23:49:41 +0900

    ADD: api 라우트 그룹 추가

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,20 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group([
+    'domain'    => config('project.api_domain'),
+    'namespace' => 'Api',
+    'as'        => 'api'
+], function () {
+    // 인증 관련 라우트
+    Route::group([
+        'prefix' => 'v1',
+        'namespace' => 'v1',
+        'as' => 'v1'
+    ], function() {
+        // 리소스 관련 라우트
+    });
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
